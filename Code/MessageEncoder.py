17af0ae2b424f797d4e0ddae1990d2cebf2909bd
Swarnima <124855325+oringejooz@users.noreply.github.com>
2023-09-30 12:51:58 +0530

    Changed alignments

--- Code/MessageEncoder.py
+++ Code/MessageEncoder.py
@@ -1,36 +1,48 @@
 from tkinter import *
 
-
 root = Tk()
 root.geometry("500x300")
-root.resizable(0,0)
-root.title("Decoder Python World and Encoder")
+root.title("Decoder and Encoder")
 
 # Define fullscreen as a global variable
 fullscreen = False
 
+# Initial font sizes
+normal_font_size = 12
+fullscreen_font_size = 20
+
 # Custom Fonts
-custom_font_title = ("Helvetica", 20, "bold")
-custom_font_label = ("Helvetica", 12)
-custom_font_button = ("Helvetica", 10, "bold")
+custom_font_title = ("Helvetica", fullscreen_font_size, "bold")
+custom_font_label = ("Helvetica", normal_font_size)
+custom_font_button = ("Helvetica", normal_font_size, "bold")
 
 # Background Colors
 bg_color = "lightgray"
 button_bg_color = "lightblue"
 button_fg_color = "white"
 
+def center_window():
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    window_width = 500
+    window_height = 300
+
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 Label(root, text="ENCODE DECODE", font=custom_font_title).pack()
-Label(root, text="Python World", font=custom_font_title, fg="blue").pack(side=BOTTOM)
+Label(root, text="CAESAR CIPHER", font=custom_font_title, fg="blue").pack(side=BOTTOM)
 
 Text = StringVar()
-Result = StringVar()
+Result = StringVar()  # Rename EncodedText to Result
 
 def Encode(message):
-        encoded = ""
-        for char in message:
-            encoded += chr(ord(char) + 1)  # Caesar cipher with a shift of 1
-        return encoded
+    encoded = ""
+    for char in message:
+        encoded += chr(ord(char) + 1)  # Caesar cipher with a shift of 1
+    return encoded
 
 def Decode(encoded_message):
     decoded = ""
@@ -41,12 +53,12 @@ def Decode(encoded_message):
 def Mode():
     input_text = Text.get()
     encoded_text = Encode(input_text)
-    Result.set(encoded_text)
+    Result.set(encoded_text)  # Set the value of Result
 
 def DecodeText():
-    encoded_text = Text.get()
-    decoded_text = Decode(encoded_text)
-    Result.set(decoded_text)
+    input_text = Text.get()
+    decoded_text = Decode(input_text)  # Decode the input text directly
+    Result.set(decoded_text)  # Set the value of Result
 
 def Exit():
     root.destroy()
@@ -61,17 +73,54 @@ def ToggleFullscreen():
     fullscreen = not fullscreen
     root.attributes("-fullscreen", fullscreen)
 
-Label(root, font="arial 12 bold", text="MESSAGE").place(x=60, y=60)
-Entry(root, font="arial 10", textvariable=Text, bg="ghost white").place(x=290, y=60)
+    # Update font sizes based on fullscreen mode
+    if fullscreen:
+        custom_font_title = ("Helvetica", fullscreen_font_size, "bold")
+        custom_font_label = ("Helvetica", fullscreen_font_size)
+        custom_font_button = ("Helvetica", fullscreen_font_size, "bold")
+    else:
+        custom_font_title = ("Helvetica", normal_font_size, "bold")
+        custom_font_label = ("Helvetica", normal_font_size)
+        custom_font_button = ("Helvetica", normal_font_size, "bold")
 
-Entry(root, font="arial 10 bold", textvariable=Result, bg="ghost white").place(x=290, y=150)
+    # Update fonts for labels and buttons
+    for widget in root.winfo_children():
+        if isinstance(widget, Label):
+            widget.configure(font=custom_font_title)
+        elif isinstance(widget, Button):
+            widget.configure(font=custom_font_button)
 
-Button(root, font="arial 10 bold", text="ENCODE", command=Mode, bg="LightGrey", padx=2).place(x=60, y=150)
-Button(root, font="arial 10 bold", text="DECODE", command=DecodeText, bg="LightGrey", padx=2).place(x=60, y=190)
-Button(root, font="arial 10 bold", text="RESET", width=6, command=Reset, bg="LimeGreen", padx=2).place(x=80, y=230)
-Button(root, font="arial 10 bold", text="EXIT", width=6, command=Exit, bg="OrangeRed", padx=2, pady=2).place(x=180, y=230)
+    root.update_idletasks()  # Update the window size
+    center_window()  # Recenter the widgets
 
-# Full Screen Button
-Button(root, text="Max/Min Screen", width=12, command=ToggleFullscreen, bg="lightgray", font=custom_font_button).place(x=350, y=230)
+# Message Label and Entry
+Label(root, font=custom_font_label, text="MESSAGE").pack(pady=5)
+entry_message = Entry(root, font=custom_font_label, textvariable=Text, bg="ghost white")
+entry_message.pack(padx=10, pady=5)
+
+# Result Text Display (formerly Encoded Text)
+Label(root, font=custom_font_label, text="RESULT").pack(pady=5)
+entry_result = Entry(root, font=custom_font_label, textvariable=Result, bg="ghost white")
+entry_result.pack(padx=10, pady=5)
+
+# Encode Button
+Button(root, font=custom_font_button, text="ENCODE", command=Mode, bg="LightGrey", padx=5, pady=5).pack(pady=5)
+
+# Decode Button
+Button(root, font=custom_font_button, text="DECODE", command=DecodeText, bg="LightGrey", padx=5, pady=5).pack(pady=5)
+
+# Reset and Exit Buttons
+Button(root, font=custom_font_button, text="RESET", width=6, command=Reset, bg="LimeGreen", padx=5, pady=5).pack(pady=10)
+Button(root, font=custom_font_button, text="EXIT", width=6, command=Exit, bg="OrangeRed", padx=5, pady=5).pack(pady=10)
+
+# Full Screen Button (Bottom Right)
+Button(root, text="Max/Min Screen", width=12, command=ToggleFullscreen, bg="lightgray", font=custom_font_button).pack(side=BOTTOM, anchor=SE, padx=10, pady=10)
+
+# Center align all widgets horizontally
+center_window()
 
 root.mainloop()
+
+
+
+
